98284f8f035c8f4542ec3d8779dcd25fd1ab6d85
Chris.Franklin <cfrank306@gmail.com>
2021-11-28 17:34:35 -0600

    added initial files

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../styles/Header.css';
+
+function Header({setPage}) {
+    return (
+        <header className="header">
+            <div>
+                <h1>Christopher Franklin</h1>
+            </div>
+            <nav>
+                <a href="#aboutMe" onClick={() => setPage('About Me')}>About Me</a>
+                <a href="#portfolio" onClick={() => setPage('Portfolio')}>Portfolio</a>
+                <a href="#contactMe" onClick={() => setPage('Contact Me')}>Contact Me</a>
+                <a href="resume" onClick={() => setPage('Resume')}>Resume</a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,2 @@
+import React from "react";
+import resume from '../../assets/files/Modern Professional Resume.pdf'
