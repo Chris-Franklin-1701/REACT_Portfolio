9c3378d1cb338c7d93500cb864ddcf51d796d9bb
Chris.Franklin <cfrank306@gmail.com>
2021-11-28 16:40:42 -0600

    created base files for project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import aboutMe from './components/aboutMe';
+import projects from './components/projects';
+import contactMe from './components/contactMe';
+import resume from './components/resume';
+
+function App() {
+    return <aboutMe />;
+}
+
+export default App;
