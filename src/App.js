a66ce04df9780b97ff9229bbd6489ceb841c31b9
Chris.Franklin <cfrank306@gmail.com>
2021-12-02 23:33:52 -0600

    worked on App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
 import './styles/App.css'
-import AboutMe from './components/AboutMe';
+import React from 'react';
+import { useState } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
 
+
+const styles = {
+    bodyStyle: {
+        background: '#ffffff'
+    }
+}
 function App() {
-    return <AboutMe />;
+    const [page, setPage] = useState('AboutMe');
+
+    return (
+        <div>
+            <Header setPage={setPage} />
+            <div style={styles.bodyStyle}>
+                <Main page={page} />
+            </div>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
