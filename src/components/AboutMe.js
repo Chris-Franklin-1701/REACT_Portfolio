af692a29a88f69c7285168ceeaa0af260256b2c6
Chris.Franklin <cfrank306@gmail.com>
2021-12-02 23:38:11 -0600

    working on styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Profile from "../images/Profile.jpg"
 
-
+const styles = {
+    profileStyle: {
+        color: 'black',
+        background: 'green'
+    }
+}
 function AboutMe() {
     return (
-        <div>
-            <image src={Profile} />
+        <div style={styles.profileStyle}>
+            <img src={Profile} />
 
             <h1>Hi, I'm Christopher Franklin.....a Full-Stack Developer</h1>
                 <p>I am from Peoria, IL. I attended Bradley University, and received my Bachelor's degree in Business Management and Administration.  I currently live in East Peoria, IL with my amazing wife and two beautiful daughters.  Having worked in several industries on the business side, I have made a move to switch my career to become a Full-Stack Developer.  I have been building pc's since I was a kid, and have self-taught myself several programming things because I wanted to change something to my liking.
