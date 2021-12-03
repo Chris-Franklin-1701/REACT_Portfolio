a2a42088ee731758a0ccf35faadf8ea550ab1dbd
Chris.Franklin <cfrank306@gmail.com>
2021-12-02 23:52:32 -0600

    removed import from index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDom from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css'
-import reportWebVitals from './reportWebVitals';
 
 ReactDom.render(<App />, document.getElementById('root'));
